318c8083f8975d3af88377020a1618b327c4b738
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-05-12 00:35:21 +1200

    Add test for current node.

--- vars/updateSource.groovy
+++ vars/updateSource.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/groovy
 
 def call() {
+    if (${NODE_NAME}.startsWith('Ubuntu') {
+        echo 'yes matching machine!'
+    } else {
+        echo 'bummer not matched'
+    }
     dir ('./opencmiss/build/manage/release') {
         sh 'make update_all'
     }
