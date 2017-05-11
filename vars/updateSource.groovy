7cdfab4ad4f44a88a31a62677d5d6443ab8e5b9a
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-05-12 00:55:07 +1200

    Remove bad echo statement.

--- vars/updateSource.groovy
+++ vars/updateSource.groovy
@@ -5,10 +5,12 @@ import jenkins.model.Jenkins
 def call() {
     echo 'hi'
     echo '${NODE_NAME}'
-    echo ${NODE_NAME}
     def envVars = Jenkins.instance.getGlobalNodeProperties()[0].getEnvVars()
+    echo 'hi'
     def nodeName = envVars['NODE_NAME']
+    echo 'hi'
     println nodeName
+    echo 'hi'
     if (nodeName.startsWith('Ubuntu')) {
         println 'yes matching machine!'
     } else {
