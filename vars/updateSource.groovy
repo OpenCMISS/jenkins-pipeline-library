404efc71ecd0b2208dc363eedb9bd4cac2c4b39e
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-03 14:53:15 +1300

    Update updateSource.groovy

--- vars/updateSource.groovy
+++ vars/updateSource.groovy
@@ -2,13 +2,19 @@
 
 def call() {
     String nodeName = "${env.NODE_NAME}";
+    String jobName = "${env.JOB_NAME}";
+    
+    String rootName = "opencmiss";
+    if (jobName.contains("Dependencies") {
+        rootName = "opencmiss_dependencies"; 
+    }
 
     if (nodeName.startsWith("Windows")) {
-        dir('./opencmiss/build/manage/') {
+        dir("./${rootName}/build/manage/") {
             bat '"C:\\Program Files\\CMake\\bin\\cmake.exe" --build . --target update_all --config Release'
         }
     } else {
-        dir ('./opencmiss/build/manage/release') {
+        dir ("./${rootName}/build/manage/release") {
             sh 'make update_all'
         }
     }
