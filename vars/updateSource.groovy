e6da86e901500b408941ccb6d92b01e74f956694
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-29 16:56:33 +1300

    Update updateSource.groovy

--- vars/updateSource.groovy
+++ vars/updateSource.groovy
@@ -11,13 +11,25 @@ def call() {
 
     if (nodeName.startsWith("Windows")) {
         dir("./${rootName}/build/manage/") {
-            bat '"C:\\Program Files\\CMake\\bin\\cmake.exe" --build . --target update_all --config Release'
+            try {
+                bat '"C:\\Program Files\\CMake\\bin\\cmake.exe" --build . --target update_all --config Release'
+            } catch (Exception ex) {
+                echo 'Error trying to update_all.'
+            }
         }
         dir("./${rootName}/src/cmake_modules/") {
-            bat 'git pull'
+            try {
+                bat 'git pull'
+            } catch (Exception ex) {
+                echo 'Error trying to update CMake modules.'
+            }
         }
         dir("./${rootName}/src/manage/") {
-            bat 'git pull'
+            try {
+                bat 'git pull'
+            } catch (Exception ex) {
+                echo 'Error trying to update manage.'
+            }
         }
     } else {
         dir ("./${rootName}/build/manage/release") {
@@ -28,10 +40,18 @@ def call() {
             }
         }
         dir("./${rootName}/src/cmake_modules/") {
+            try {
                 sh 'git pull'
+            } catch (Exception ex) {
+                echo 'Error trying to update CMake modules.'
+            }
         }
         dir("./${rootName}/src/manage/") {
+            try {
                 sh 'git pull'
+            } catch (Exception ex) {
+                echo 'Error trying to update manage.'
+            }
         }
     }
 }
