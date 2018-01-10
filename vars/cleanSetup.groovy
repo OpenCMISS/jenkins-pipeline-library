f5abbfb4103b2303a76738bdee9cf7499d923a9e
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-01-10 15:03:30 +1300

    Update cleanSetup.groovy

--- vars/cleanSetup.groovy
+++ vars/cleanSetup.groovy
@@ -1,16 +1,17 @@
 #!/usr/bin/groovy
 
 def call(String rootName) {
+    def workspace = pwd()
     dir ('./setup-build') {
         echo 'delete setup-build'
         deleteDir()
     }
     dir ('./${rootName}/build') {
-        echo 'delete ${directory}/build'
+        echo "delete ${workspace}/${rootName}/build"
         deleteDir()
     }
     dir ('./${rootName}/install') {
-        echo 'delete ${directory}/install'
+        echo "delete ${workspace}/${rootName}/install"
         deleteDir()
     }
 }
